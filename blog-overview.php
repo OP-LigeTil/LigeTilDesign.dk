2ba99818e7832bd7fbdfe266ff33b56faf8e9be5
OPanthera <101906645+OPanthera@users.noreply.github.com>
2025-09-02 12:22:35 +0200

    Remove inline styles from hero template for improved maintainability

--- blog-overview.php
+++ blog-overview.php
@@ -12,7 +12,7 @@ $image_two_column = get_field('image_two_column_text_image');
 
 <?php get_header(); ?>
 <main>
-    <div class= "hero-template-LigeTil" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/OmLigeTil-background-img.png'); background-size: cover; background-repeat: no-repeat";>
+    <div class= "hero-template-LigeTil">
         <h1><?php the_title(); ?> </h1>
     </div>
 
