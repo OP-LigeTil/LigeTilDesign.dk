a6d6a73de1d86e77073d1cc97e297926525e81e7
OPanthera <101906645+OPanthera@users.noreply.github.com>
2025-09-02 22:34:25 +0200

    Refactor single.php structure and enhance style.css for improved content layout and alignment

--- single.php
+++ single.php
@@ -9,7 +9,9 @@
         <h1 class="h-black"><?php the_title();?></h1>
         <p id="blogdate"><?php echo get_the_date('d M. o');?></p>
     </div>
+    <div class= "blog-post-content-bottom">
     <?php echo the_content();?>
+    </div>
 
 </section>
 </main>
